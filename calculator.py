12f1f48e2864a8be20633499a31a0d88a06a486d
yamini827 <venutharlayamini@gmail.com>
2025-08-04 21:46:20 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,48 @@
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero not possible!"
+    return x / y
+def calculator():
+    while True:
+        print("\n_Basic Calculator_")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
+
+        choice = input("Enter your option (1-5): ")
+
+        if choice == '5':
+            print("Exiting from the calculations !")
+            break
+
+        if choice in ['1', '2', '3', '4']:
+            try:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+            except ValueError:
+                print("Invalid input. Please enter number.")
+                continue
+
+            if choice == '1':
+                print("Result:", add(num1, num2))
+            elif choice == '2':
+                print("Result:", subtract(num1, num2))
+            elif choice == '3':
+                print("Result:", multiply(num1, num2))
+            elif choice == '4':
+                print("Result:", divide(num1, num2))
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+
+calculator()
+            
+            
